5aff3f95d3cae5b3cfb424adca565b2693c3879c
gonced8 <gonced8@gmail.com>
2019-10-21 14:06:04 +0100

    Open the input file

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
 #!env/bin/python3.7
 
+import sys
+
 import search
 
 
@@ -7,5 +9,23 @@ class ASARProblem(search.Problem):
     pass
 
 
+def get_argv():
+    if len(sys.argv)>1:
+        filename = sys.argv[1]
+    else:
+        print(sys.argv[0]+" <input file>")
+        sys.exit(-1)
+
+    return filename
+
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        pass
+
+
 if __name__ == '__main__':
-    pass
+    filename = get_argv()
+    read_input()
+    
+
