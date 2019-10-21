a30b83efcd91cc58bef6caefaf5c8f42cc890f31
gonced8 <gonced8@gmail.com>
2019-10-21 17:06:55 +0100

    Processes input file

--- solution.py
+++ solution.py
@@ -20,12 +20,46 @@ def get_argv():
 
 
 def read_input(filename):
+    A = []
+    C = []
+    P = []
+    L = []
+
     with open(filename, 'r') as f:
-        pass
+        for line in f.readlines():
+            splitted = line.split()
+            if not splitted:
+                continue
+
+            code = splitted[0]
+            arg = splitted[1:]
+
+            if code == 'A':
+                d = dict(zip(["code", "start", "end"], arg))
+                A.append(d)
+
+            elif code == 'C':
+                d = dict(zip(["class", "dr"], arg))
+                C.append(d)
+
+            elif code == 'P':
+                d = dict(zip(["airplane", "class"], arg))
+                P.append(d)
+
+            elif code == 'L':
+                d = dict(zip(["dep", "arr", "dl"], arg))
+                d.update({ arg[i]: arg[i+1] for i in range(3, len(arg), 2) })
+                L.append(d)
+
+    return A, C, P, L
 
 
 if __name__ == '__main__':
     filename = get_argv()
-    read_input()
+    A, C, P, L = read_input(filename)
+    print(A, '\n')
+    print(C, '\n')
+    print(P, '\n')
+    print(L, '\n')
     
 
